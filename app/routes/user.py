46eac8cb7e5a84d472ecc6fa7d92d57cd690bb1f
some-repository <patch.code2002@gmail.com>
2025-05-05 23:52:19 +0300

    Added read_users endpoint

--- app/routes/user.py
+++ app/routes/user.py
@@ -1,8 +1,8 @@
 # app/routes/user.py
 """Module process user CRUD requests"""
 
-from fastapi import APIRouter, status, Depends
-from sqlmodel import Session
+from fastapi import APIRouter, status, Depends, HTTPException
+from sqlmodel import Session, select
 from app.db import get_session
 from ..schemas import action_data_structure as schema_structures
 
@@ -22,3 +22,16 @@ def create_user (user: schema_structures.user_create,
     session.commit ()
     session.refresh (new_user)
     return new_user
+
+# get users list
+@router.get ("/", status_code = status.HTTP_200_OK,
+                  response_model = list [schema_structures.user_read])
+def read_users (session: Session = Depends (get_session)):
+    users = session.exec (select (schema_structures.User)).all ()
+
+    if users is None or len (users) == 0:
+        raise HTTPException (
+            status_code = status.HTTP_204_NO_CONTENT,
+            detail = "The user list is empty"
+        )
+    return users
