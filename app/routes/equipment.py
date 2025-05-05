bbdb53863c85dd57a9c18a393112a47195a06e91
some-repository <patch.code2002@gmail.com>
2025-05-05 23:58:59 +0300

    Added read_equipments endpoint

--- app/routes/equipment.py
+++ app/routes/equipment.py
@@ -1,8 +1,8 @@
 # app/routes/equipment.py
 """Module process equipment CRUD requests"""
 
-from fastapi import APIRouter, status, Depends
-from sqlmodel import Session
+from fastapi import APIRouter, status, Depends, HTTPException
+from sqlmodel import Session, select
 from app.db import get_session
 from ..schemas import action_data_structure as schema_structures
 
@@ -22,3 +22,16 @@ def create_equipment (equipment: schema_structures.equipment_create,
     session.commit ()
     session.refresh (new_equipment)
     return new_equipment
+
+# get equipments list
+@router.get ("/", status_code = status.HTTP_200_OK,
+                  response_model = list [schema_structures.equipment_read])
+def read_equipments (session: Session = Depends (get_session)):
+    equipments = session.exec (select (schema_structures.Equipment)).all ()
+
+    if equipments is None or len (equipments) == 0:
+        raise HTTPException (
+            status_code = status.HTTP_204_NO_CONTENT,
+            detail = "The equipment list is empty"
+        )
+    return equipments
